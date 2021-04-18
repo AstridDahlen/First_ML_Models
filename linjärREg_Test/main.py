ea22fa7398aa46c30e304caf50b6fc44f5d1ed1c
astriddahlen <astriddahlen@hotmail.com>
2021-04-18 15:59:14 +0200

    decision tree first test

--- linjärREg_Test/main.py
+++ linjärREg_Test/main.py
@@ -13,11 +13,19 @@ df = pd.read_csv('uppdaterad.csv')
 pris_per_år = df.groupby('år').Fastighetsprisbostadsrättkronorkvm.mean().reset_index()
 #define X variable as year and reshape
 X = pris_per_år['år']
+
+# -1 keeps element as they are, 1 , a new dimension is added when you put in 1
 X = X.values.reshape(-1, 1)
 #define y as price  square foot in kronor
 y = pris_per_år['Fastighetsprisbostadsrättkronorkvm']
 # plot
 plt.scatter(X,y)
+
+print(X.ndim)
+print(y.ndim)
+print(y.shape)
+print(X.shape)
+
 # apply regression
 
 regr = linear_model.LinearRegression()
@@ -28,8 +36,8 @@ regr.fit(X,y)
 y_predict = regr.predict(X)
 # plot prediction
 plt.plot(X,y_predict)
-X_future = np.array(range(2019,2030))
-X_future= X_future.reshape(-1, 1)
+X_future = np.array(range(2019,2025))
+X_future= X_future.reshape(-1,1)
 future_predict = regr.predict(X_future)
 print(future_predict)
 plt.plot(X_future,future_predict)
